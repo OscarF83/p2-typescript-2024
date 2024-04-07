a3b1d4e05c4dea6da1148e5835cc0b327f0987a3
Oscar Fonte <o_fonte@hotmail.com>
2024-04-07 17:39:58 +0200

    Dando estilo a Index.html

--- renderMainHtml.ts
+++ renderMainHtml.ts
@@ -5,13 +5,16 @@ const renderCountries = (countries: Array<Country>) => {
   for (const country of countries) {
     html =
       html +
-      `<div><img src="${country.flags.png}" /></div>
-        <div>${country.nameCountry}</div>
-    <div>${country.capitalCountry}</div>
-    <div> <p><a href=/${country.nameCountry.replace(
-      / /g,
-      ""
-    )}.html> More details... </a></p></div>`;
+      `<div class="country"><img src="${country.flags.png}" />
+      <div class="info">
+        <div class="name">${country.nameCountry}</div>
+        <div class="capital">${country.capitalCountry}</div>
+        <div> <p><a href=/${country.nameCountry.replace(
+          / /g,
+          ""
+        )}.html> More details... </a></p></div>
+      </div>
+      </div>`;
   }
   return html;
 };
@@ -22,6 +25,35 @@ export const renderMainHtml = (countries: Array<Country>) => {
   <head>
   <meta charset="UTF-8">
   <title>Countries List</title>
+  <style>
+  body{
+    background-color: rgb(20,20,20);
+    color: white;
+  }
+  .country {
+    display: flex;
+    flex-direction: row;
+    padding: 1em;
+  }
+  .country img{
+    border: 1px solid black;
+  }
+  .info{
+    display: flex;
+    flex-direction: column;
+    padding: 1em;
+    margin-left: 1em;
+    margin-top: 3em;
+  }
+  .name{
+    font-size: large;
+    font-weight: bold;
+    padding-bottom: 1em;
+  }
+  a {
+    color: pink;
+  }
+  </style>
 </head>
   <body>
     ${renderCountries(countries)}
