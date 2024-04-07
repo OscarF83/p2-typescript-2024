e069a2024b30b0e0d63f580a9268a7faf7c3d661
Oscar Fonte <o_fonte@hotmail.com>
2024-04-07 19:04:43 +0200

    Aplicación lista devuelve lista y crea html de 50 countries

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { loadCountries } from "./countries.js";
 import { renderMainHtml } from "./renderMainHtml.js";
 import { renderCountryHtml } from "./renderCountryHtml.js";
 
-const countries = await loadCountries(6);
+const countries = await loadCountries(50);
 const html = renderMainHtml(countries);
 renderCountryHtml(countries);
 await writeFile("index.html", html);
